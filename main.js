0e88e5147c9092be3a5a74610a6e055bea6ba799
MahmoudEmawi7122 <103826756+MahmoudEmawi7122@users.noreply.github.com>
2022-04-21 16:04:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let BlockChain = require("./BlockChain");
 let blockchain = new BlockChain();
 let hash = require('object-hash');
 let PROOF = 156;
-
 if(proofOfWork() == PROOF){
     blockchain.addNewTransaction("Mahmoud","Emawi",120);
     let previousHash = blockchain.lastBlock() ? blockchain.lastBlock().hash : null;
